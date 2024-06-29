a05d3a54ba449b0caea72fef3bb3c4b6cbda7b9a
Vladimir Revyagin <skorabeyf@gmail.com>
2024-06-29 14:10:11 +0300

    feat(useViewportSize): Рабочий код

--- src/hooks/useViewportSize.js
+++ src/hooks/useViewportSize.js
@@ -1,11 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function useViewportSize() {
-  const [height] = useState('')
-  const [width] = useState('')
+  let defaultSize = window.visualViewport
+  let [size, setSize] = useState(() =>
+    defaultSize ? { width: 0, height: 0 } : getViewportSize()
+  )
 
+  useEffect(() => {
+    let onResize = () => {
+      setSize((size) => {
+        let newSize = getViewportSize()
+        if (newSize.width === size.width && newSize.height === size.height) {
+          return size
+        }
+        return newSize
+      })
+    }
+
+    if (!visualViewport) {
+      window.addEventListener('resize', onResize)
+    } else {
+      visualViewport.addEventListener('resize', onResize)
+    }
+
+    return () => {
+      if (!visualViewport) {
+        window.removeEventListener('resize', onResize)
+      } else {
+        visualViewport.removeEventListener('resize', onResize)
+      }
+    }
+  }, [])
+
+  return size
+}
+function getViewportSize() {
   return {
-    height,
-    width,
+    width: (visualViewport && visualViewport?.width) || window.innerWidth,
+    height: (visualViewport && visualViewport?.height) || window.innerHeight,
   }
 }
